119d4bb8b736c93e6228d0f647e01f0728827eb2
Ashish <81252055+ashishbdhane@users.noreply.github.com>
2022-12-16 14:36:46 +0530

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 // @author: stash ¯\_(ツ)_/¯
+#define FAST cin.tie(0), cout.tie(0),ios_base::sync_with_stdio(false);
+#define int long long
 
-#define ll long long
-
-int main()
+int32_t main()
 {
-    cin.tie(0), cout.tie(0),ios_base::sync_with_stdio(false);
-    ll tt;cin >> tt;
+    FAST
+    int tt;cin >> tt;
     while(tt--)
     {
         
