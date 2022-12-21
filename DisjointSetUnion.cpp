f6f4a2843eacfe4c49bea48231692c4395e1e729
Ashish <81252055+ashishbdhane@users.noreply.github.com>
2022-12-21 21:40:27 +0530

    Added unionBySize and Simplified

--- DisjointSetUnion.cpp
+++ DisjointSetUnion.cpp
@@ -1,45 +1,40 @@
-// @author: stash ¯\_(ツ)_/¯
-
-class DisjointSet{
-public:
-    vector<int> size,mini,maxi,parent,rank;
-    DisjointSet(int n)
-    {
+class DisjointSet {
+    vector<int> rank, parent, size; 
+public: 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
         parent.resize(n+1);
-        size.resize(n+1);
-        rank.resize(n+1);
-        for(int i=0;i<=n;i++)
-        {
-            parent[i]=i;
-            rank[i]=1;
-            size[i]=1;
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
         }
     }
 
-    int get(int node)
-    {
-        return parent[node]=((node==parent[node])?node:get(parent[node]));
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
     }
 
-    void unionByRank(int u,int v)
-    {
-        u=get(u);
-        v=get(v);
-        if(u==v)
-            return;
+    void unionByRank(int u, int v) {
+        u = findUPar(u); 
+        v = findUPar(v); 
+        if(v == u) return;
         if(rank[u]==rank[v])
-        {
             rank[u]++;
-        }
-        if(rank[u]>rank[v])
-        {
-            size[u]+=size[v];
-            parent[v]=u;
-        }
-        else
-        {
-            size[v]+=size[u];
-            parent[u]=v;
-        }
-    }  
+        if(rank[u]<rank[v])swap(u,v);
+        
+        parent[v] = u; 
+    }
+
+    void unionBySize(int u, int v) {
+        u = findUPar(u); 
+        v = findUPar(v); 
+        if(u == v) return; 
+        if(size[u]<size[v]) swap(u,v);
+        
+        parent[v] = u;
+        size[u] += size[v]; 
+    }
 };
