7c24aea1eb29f671bf5b0698361ff52a6b738704
Ashish <81252055+ashishbdhane@users.noreply.github.com>
2022-12-21 22:22:49 +0530

    "\n

--- basic.cpp
+++ basic.cpp
@@ -4,6 +4,7 @@ using namespace std;
 // @author: stash ¯\_(ツ)_/¯
 #define FAST cin.tie(0), cout.tie(0),ios_base::sync_with_stdio(false);
 #define int long long
+#define endl "\n"
 
 int32_t main()
 {
