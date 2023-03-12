629eef3269c7770e3c14c0bd5fcbbeb5708d937f
Ashish <81252055+ashishbdhane@users.noreply.github.com>
2023-03-12 15:32:57 +0530

    Updated

--- basic.cpp
+++ basic.cpp
@@ -4,6 +4,9 @@ using namespace std;
 // @author: stash ¯\_(ツ)_/¯
 #define FAST cin.tie(0), cout.tie(0),ios_base::sync_with_stdio(false);
 #define int long long
+#define debug(x) cerr << "[" <<#x << " = " << x << "]" << endl
+#define debug2(x, y) cerr << "[" <<#x << " = " << x << "]" << ", [" <<#y << " = " << y << "]" << endl
+#define all(x) (x).begin(),(x).end()
 #define endl "\n"
 
 int32_t main()
